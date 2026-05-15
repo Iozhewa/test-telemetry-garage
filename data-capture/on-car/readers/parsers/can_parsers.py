9a80c198f926fbfca9b25cf5b28e7b51a97703cb
Yeshua Dela Pena <u7695974@anu.edu.au>
2026-05-15 12:31:09 +1000

    Implemented BPS, APPS parsers

--- data-capture/on-car/readers/parsers/can_parsers.py
+++ data-capture/on-car/readers/parsers/can_parsers.py
@@ -25,3 +25,39 @@ def test_sensor():
     result = parse_sensor(testBytes)
     print(f"Sensor data: {result} units")
     assert result == 75  # Or other expected value
+
+def parse_brake_pressure(data:bytes) -> float:
+    """
+    Brake Pressure Parser (for BPS)
+    Expects _ bytes. Returns bar.
+    https://www.bestech.com.au/blogs/how-pressure-sensors-are-used-in-hydraulic-brake-test-setup/
+
+    Verify against ECU datasheet
+    """
+    raw = data[0]
+    toBar = lambda psi: psi / 14.504
+    standard = toBar(raw)
+    return standard
+def test_brake_pressure():
+    testBytes = bytes([14, 28])
+    result = parse_brake_pressure(testBytes)
+    print(f"Brake Pressure: {round(result, 2)} bar")
+    assert round(result) == 1.0, f"Result was {round(result, 2)} and not ~1"
+
+def parse_accelerator_pedal_position(data:bytes) -> float:
+    """
+    Accelerator Pedal Position Parser (for APPS)
+    Expects _ bytes. Returns pedal travel (0-100%).
+    https://filmelectronics.in/accelerator-pedal-position-sensor-importance/
+
+    Verify against ECU datasheet
+    """
+    raw = data[0]
+    toTravel = lambda volts: 100*(volts/5)
+    standard = toTravel(raw)
+    return standard
+def test_accelerator_pedal_position():
+    testBytes = bytes([1, 3])
+    result = parse_accelerator_pedal_position(testBytes)
+    print(f"Accelerator Pedal Position: {round(result, 2)}%")
+    assert round(result) == 20.0, f"Result was {round(result, 2)} and not ~20"
