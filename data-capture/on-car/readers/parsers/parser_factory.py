6625675b4f5d42ee07f9097c4c52d4723a19c4e4
Yeshua Dela Pena <u7695974@anu.edu.au>
2026-05-15 13:35:13 +1000

    Introduced parser_factory and destination file parser_product for an automated process of parser coding

---
+++ data-capture/on-car/readers/parsers/parser_factory.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''
+Two ways of completing Task A
+    [1] Zone out and repeat a process of filling a template, researching missing details, and mocking up a test
+        - Doing this, I forgot to check if any were IMU-connected
+    [2] Attempt a parser factory that applies a generic template, to edit later
+        - I had a draft of this on a cloned repository I forgot to save somewhere
+        - This is the 2nd attempt at doing this. That draft was buggy anyway.
+    
+    Yes I did [1] already, but [2] would be good to have on standby.
+'''
+import csv
+from io import StringIO
+
+if __name__ == "__main__":
+    '''
+    access_sensor_table()
+    filter_connected_sensors()
+    madlib_parser_functions()
+    dump_functional_code()
+    classify_parsers()
+    '''
---
+++ data-capture/on-car/readers/parsers/parser_product.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Sensor translators.
+
+Each function to take CAN message in raw bytes, decoding
+measured values (degrees, kPa, rpm, etc.)
+
+Working with placeholder byte formats until Database CAN
+file is provided (that is, need more ECU info).
+"""
