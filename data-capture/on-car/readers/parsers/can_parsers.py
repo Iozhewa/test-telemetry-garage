f1a2bfb98ed4fe1c637a63c174a7483c3b62e813
Yeshua Dela Pena <u7695974@anu.edu.au>
2026-05-15 12:31:58 +1000

    Implemented SAS, SPS, WS parsers

--- data-capture/on-car/readers/parsers/can_parsers.py
+++ data-capture/on-car/readers/parsers/can_parsers.py
@@ -61,3 +61,57 @@ def test_accelerator_pedal_position():
     result = parse_accelerator_pedal_position(testBytes)
     print(f"Accelerator Pedal Position: {round(result, 2)}%")
     assert round(result) == 20.0, f"Result was {round(result, 2)} and not ~20"
+
+def parse_steering_angle(data:bytes) -> float:
+    """
+    Steering Angle Parser (for SAS)
+    Expects _ bytes. Returns turn (1 tr = 360 deg).
+    https://www.delphiautoparts.com/resource-center/article/making-sense-of-sensors-steering-angle-sensor
+
+    Verify against ECU datasheet
+    """
+    raw = data[0]
+    toTurn = lambda degrees: degrees/360
+    standard = toTurn(raw)
+    return standard
+def test_steering_angle():
+    testBytes = bytes([45, 90])
+    result = parse_steering_angle(testBytes)
+    print(f"Steering Angle: {round(result, 2)} turn")
+    assert result == 0.125, f"Result was {round(result, 3)} and not ~0.125"
+
+def parse_suspension_potentiometer(data:bytes) -> float:
+    """
+    Suspension Potentiometer Parser (for SP)
+    Expects _ bytes. Returns cm.
+    https://www.datamc.org/data-acquisition/adding-sensors/suspension-potentiometers/
+
+    Verify against ECU datasheet
+    """
+    raw = data[0]
+    toCm = lambda mm: mm / 10
+    standard = toCm(raw)
+    return standard
+def test_suspension_potentiometer():
+    testBytes = bytes([10, 20])
+    result = parse_suspension_potentiometer(testBytes)
+    print(f"Suspension Potentiometer: {round(result, 2)} cm")
+    assert result == 1.0, f"Result was {round(result, 2)} and not ~0.1"
+
+def parse_wheel_speed(data:bytes) -> float:
+    """
+    Wheel Speed Parser (for WS)
+    Expects _ bytes. Returns m/s.
+    https://premierautotrade.com.au/news/wheel-speed-sensors%E2%80%93more-than-just-abs.php
+
+    Verify against ECU datasheet
+    """
+    raw = data[0]
+    toMS = lambda kmh: kmh / 3.6
+    standard = toMS(raw)
+    return standard
+def test_wheel_speed():
+    testBytes = bytes([1, 10])
+    result = parse_wheel_speed(testBytes)
+    print(f"Wheel Speed: {round(result, 2)} m/s")
+    assert round(result, 3) == round(0.277778, 3), f"Result was {round(result, 3)} and not ~{round(0.277778, 3)}"
